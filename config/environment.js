d32b78160dabd3c73b2f9b779f05c9c30ba8793d
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-28 17:49:34 -0700

    add initial files

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rentals',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://blazing-heat-7712.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
